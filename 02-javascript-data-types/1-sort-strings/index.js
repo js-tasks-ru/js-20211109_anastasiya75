7e0937302e90e9c5e4a5e36d6cb1d1e6aad4453c
Anastasia Kalinina <a.kalinina@eapteka.ru>
2021-11-13 20:06:36 +0500

    Написана функция сортировки строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,24 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrNormalize = arr.map(item => item.normalize());
+  const compareFn = (a, b) => a.localeCompare(b, ['ru', 'en'], {sensitivity: 'variant', caseFirst: 'upper'});
+  const resArr = arrNormalize.sort(compareFn);
 
+  if (param === 'asc') {
+    return resArr;
+  }
+
+  if (param === 'desc') {
+    return resArr.reverse();
+  }
 }
+
+
+
+
+
+
+
+
+
